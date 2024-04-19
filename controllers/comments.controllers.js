a455a45b45e37c63c4d31e2ee7d26b3790c926eb
Filipe da Silva <silvafilipeuk@gmail.com>
2024-04-19 17:24:21 +0100

    Task 21 - Pagination for comments implemented, with limit and p queries.

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -9,9 +9,10 @@ const { fetchArticlesById } = require("../models/articles.models");
 
 function getCommentsByArticleId(req, res, next) {
 	const { article_id } = req.params;
+	const { limit, p } = req.query;
 
 	Promise.all([
-		fetchCommentsByArticleId(article_id),
+		fetchCommentsByArticleId(article_id, limit, p),
 		fetchArticlesById(article_id),
 	])
 		.then(([comments]) => {
