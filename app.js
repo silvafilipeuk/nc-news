527d8abd41052c401de6ee781ecf9246f5d56d1e
Filipe da Silva <silvafilipeuk@gmail.com>
2024-05-06 14:30:14 +0100

    allow cross origin resource sharing.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const { apiRouter } = require("./routes/api-router");
+const cors = require("cors");
 
 const {
 	handleCustomErrors,
@@ -9,6 +10,7 @@ const {
 } = require("./middlewares/errorHandling");
 
 app.use(express.json());
+app.use(cors());
 
 // Router:
 app.use("/api", apiRouter);
